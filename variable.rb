e6e890e31a533947b87487bf214febd9494c45eb
OONOSSHINPEI <110380501+OONOSSHINPEI@users.noreply.github.com>
2022-08-08 01:25:33 +0000

    [Finish]lesson6

---
+++ variable.rb
@@ -0,0 +1,8 @@
+webcamp = "プログラミング学習"
+puts webcamp
+
+webcamp = "オンラインプログラミング学習"  # この行を追加
+puts webcamp  # この行を追加
+
+Pi = 3.14
+puts Pi
---
+++ weight.rb
@@ -0,0 +1,4 @@
+name = "A"
+weight = 50
+push name + "さんの体重は" + weight.to_s + "kgです"
+puts "#{name}さんの体重は#{weight}kgです"
